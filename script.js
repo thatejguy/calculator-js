b444cf6b3fa80102fd7b3bfef6b1ea16ad013080
thatejguy <ejgrotto@gmail.com>
2023-09-01 17:57:49 +1000

    Added functions

---
+++ script.js
@@ -0,0 +1,22 @@
+//logic functions
+const logic = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    return a / b;
+  }
+};
+
+//variables for calculator
+let num1;
+let num2;
+let op;
+
+//operate
