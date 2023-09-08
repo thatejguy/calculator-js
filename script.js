46a72ddabedfd1c5087f93a67cffc654a23c089b
thatejguy <ejgrotto@gmail.com>
2023-09-09 00:46:21 +1000

    added more css

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-//logic functions
+//operators
 const logic = {
   add(a, b) {
     return a + b;
-  },
-  subtract(a, b) {
-    return a - b;
-  },
-  multiply(a, b) {
-    return a * b;
-  },
-  divide(a, b) {
-    return a / b;
   }
-};
-
-//variables for calculator
-let num1;
-let num2;
-let op;
-
-//operate
+}
