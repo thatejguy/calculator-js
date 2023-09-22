1bbfb01479334ad45dc0d13b4da1fbc261a0c779
thatejguy <ejgrotto@gmail.com>
2023-09-22 21:55:09 +1000

    added functions and objects

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-//operators
+//selectors
+const display = document.querySelector('.display')
+
+//logic
 const logic = {
   add(a, b) {
     return a + b;
-  }
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
 }
+
+//buttons
+const buttons = {
+  one() {
+    let one = document.querySelector('.one');
+    one.addEventListener('click', () => {
+      console.log('one');
+      display.textContent += '1';
+    })
+  },
+  two() {
+    let two = document.querySelector('.two');
+    two.addEventListener('click', () => {
+      console.log('two');
+      display.textContent += '2';
+    })
+  },
+}
+
+buttons.two()
+
+
+
+
+
