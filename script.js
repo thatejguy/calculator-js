91fc23d37de5e550e1467f1048066b97c778dbf9
thatejguy <ejgrotto@gmail.com>
2023-09-25 15:21:29 +1000

    variables

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //selectors
 const display = document.querySelector('.display')
 
+//variables
+let firstNumber;
+let lastNumber;
+let operator;
+
 //logic
 const logic = {
   add(a, b) {
@@ -30,6 +35,21 @@ buttons.forEach(function(button) {
     } else if (button.textContent === '3') {
       console.log('Button Three');
       return display.textContent += '3';
+    } else if (button.textContent === '4') {
+      console.log('Button Four');
+      return display.textContent += '4';
+    } else if (button.textContent === '5') {
+      console.log('Button Five');
+      return display.textContent += '5';
+    } else if (button.textContent === '6') {
+      console.log('Button Six');
+      return display.textContent += '6';
+    } else if (button.textContent === '7') {
+      console.log('Button Seven');
+      return display.textContent += '7';
+    } else if (button.textContent === '8') {
+      console.log('Button Eight');
+      return display.textContent += '8';
     }
   })
 })
