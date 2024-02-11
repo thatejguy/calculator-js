c4210c834942932529dbeb3859fea2af79bd3f00
thatejguy <ejgrotto@gmail.com>
2024-02-11 22:34:37 +1100

    changed some stuff lol

---
+++ script.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+  let result = a + b;
+  console.log(result);
+  return result;
+}
+
+function subtract(a, b) {
+  let result = a - b;
+  console.log(result);
+  return result;
+}
