8cb49fa4e624a7fccac0b5962ad52404c891a3e7
thatejguy <ejgrotto@gmail.com>
2024-02-19 21:48:53 +1100

    added query selectors

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ function subtract(a, b) {
   console.log(result);
   return result;
 }
+
+//selectors
+let display = document.querySelector('.display');
+
+
+//buttons
+let clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+  display.textContent = 0;
+})
+
+
+//numbers
+let one = document.getElementById("one");
+one.addEventListener('click', () => {
+  if (display.textContent == 0) {
+    display.textContent = 1;
+  } else {
+    display.textContent += 1;
+  }
+})
+let two = document.getElementById('two');
+two.addEventListener('click', () => {
+  if (display.textContent == 0) {
+    display.textContent = 2;
+  } else {
+    display.textContent += 2;
+  }
+})
